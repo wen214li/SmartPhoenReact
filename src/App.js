eb8268e19cf1bdfc584649c2d5aecd20507ee330
李雯 <wen@ris-MacBook-Pro.local>
2019-11-26 14:17:58 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import DashBoard from './components/Dashboard.js';
+
+function App() {
+  return (
+    <DashBoard/>
+  );
+}
+
+export default App;
